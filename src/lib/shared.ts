6959c46635a15819e7dd2c493518e1c7ac9f53bb
Adrià <adriablancafort@gmail.com>
2026-08-05 15:10:46 -0700

    rename

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,11 +1,11 @@
-export const appName = 'Tanstack Start';
+export const appName = 'Docs Template';
 export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
+  user: 'adriablancafort',
+  repo: 'docs-template',
   branch: 'main',
 };
 

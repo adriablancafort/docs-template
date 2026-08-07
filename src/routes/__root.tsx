42c0f185e725aed7c64e76914bc4eb94ea09df27
Adrià <adriablancafort@gmail.com>
2026-08-07 15:30:48 -0700

    add favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,7 +23,10 @@ export const Route = createRootRoute({
         title: appName,
       },
     ],
-    links: [{ rel: "stylesheet", href: appCss }],
+    links: [
+      { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+    ],
   }),
   component: RootComponent,
 });
